5fc4b537d5166088878d2fc0587f0b0b84516ea1
fadimrb <130900319+fadimrb@users.noreply.github.com>
2023-04-28 11:30:39 +0300

    Update IFID.vhd

--- IFID.vhd
+++ IFID.vhd
@@ -7,6 +7,7 @@ entity IFID is
     port(
         clk: in std_logic;
         rst: in std_logic;
+--        IFIDWrite: in std_logic;
         PCin: in std_logic_vector(XLEN-1 downto 0);
         IMEMin: in std_logic_vector(XLEN-1 downto 0);
         PCout: out std_logic_vector(XLEN-1 downto 0);
@@ -16,17 +17,19 @@ end IFID;
 
 architecture IFID of IFID is
 begin
-    process(clk,rst) is 
+    process(clk,rst) is --IFIDWrite
     begin
+--        if IFIDWrite = '1' then
+--            PCOut <= (others => '0');
+--            IMEMOut <= (others => '0');
+--        else
         if(rst = '0') then 
             PCOut <= (others => '0');
             IMEMout <= (others => '0');
         elsif rising_edge(clk) then 
             PCout <= PCin;
             IMEMout <= IMEMin;
-        else then 
-            PCOut <= (others => '0');
-            IMEMout <= (others => '0');
         end if;
+--        end if;
     end process;
 end architecture;
